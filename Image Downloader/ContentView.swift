0640d7ab64492ae64a45183259536f993cc34bfe
iBenzene <2026429822@qq.com>
2023-12-26 15:14:04 +0800

    Initial Commit

---
+++ Image Downloader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Image Downloader
+//
+//  Created by 邱想想 on 2023/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Image Downloader/Image_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_DownloaderApp.swift
+//  Image Downloader
+//
+//  Created by 邱想想 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Image_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
